e5881668a577fb2e1c2212f4982889054bb18a94
Alibek <alibek80@gmail.com>
2019-01-05 00:50:36 +0600

    The last commit

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ namespace Snake
 	{
 		static void Main( string[] args )
 		{
-			//Console.SetWindowSize( 80, 25);
-            //Console.SetBufferSize( 80, 25 );
+			// Console.SetBufferSize( 80, 25 );
 
 			Walls walls = new Walls( 80, 25 );
 			walls.Draw();
@@ -48,7 +47,30 @@ namespace Snake
 					snake.HandleKey( key.Key );
 				}
 			}
+			WriteGameOver();
+			Console.ReadLine();
       }
+
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition( xOffset, yOffset++ );
+			WriteText( "============================", xOffset, yOffset++ );
+			WriteText( "И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++ );
+			yOffset++;
+			WriteText( "Автор: Евгений Картавец", xOffset + 2, yOffset++ );
+			WriteText( "Специально для GeekBrains", xOffset + 1, yOffset++ );
+			WriteText( "============================", xOffset, yOffset++ );
+		}
+
+		static void WriteText( String text, int xOffset, int yOffset )
+		{
+			Console.SetCursorPosition( xOffset, yOffset );
+			Console.WriteLine( text );
+		}
 
 	}
 }
